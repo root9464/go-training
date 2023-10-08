abb73ac9bf10d78913874a805ae695844a110a6f
root9464 <senpai9464@yandex.ru>
2023-10-08 22:04:34 +0500

    08.10.2023

--- go.dir1/1.go
+++ go.dir1/1.go
@@ -1,9 +1,20 @@
+//рекурсия
 
 package main
+
 import (
 	"fmt"
 )
-func main() {
-	fmt.Println("начало работы")
+
+func pow(x, n int) []int {
+	var result int = 1
+
+	for i := 0; i < n; i++ {
+		result *= x
+	}
+	return []int{result}
 }
-	
+
+func main() {
+	fmt.Println(pow(2, 3))
+}
