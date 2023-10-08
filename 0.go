2a96c535090d6930e89215c138161dce4b51811e
root9464 <senpai9464@yandex.ru>
2023-10-08 21:36:49 +0500

    08.10.2023

---
+++ 0.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+var line string = `
+package main
+import (
+	"fmt"
+)
+func main() {
+	fmt.Println("начало работы")
+}
+	`
+
+func createFile(a, b int) {
+	for i := a; i <= b; i++ {
+		path := "dir" + strconv.Itoa(i)
+		filename := filepath.Join(path, strconv.Itoa(i)+".go")
+		os.Mkdir(path, os.ModeDir|0755)
+
+		file, err := os.Create(filename)
+		file.WriteString(line)
+		if err != nil {
+			panic(err)
+		}
+		file.Close()
+
+	}
+	fmt.Println("Сделано епт")
+}
+
+func main() {
+	createFile(2, 3)
+}
